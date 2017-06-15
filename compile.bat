5698cc3a4a097a4c549a5b888bcd97288776b5b4
Randolph Huang <wolftotem4@gmail.com>
2017-06-15 17:40:53 +0800

    2017/6/15

--- compile.bat
+++ compile.bat
@@ -1,10 +1,12 @@
 java -jar .\third_party\closure\closure-compiler.jar ^
-  --js node_modules/google-closure-library/closure/goog/base.js ^
-  --js lib/**.js ^
-  --dependency_mode STRICT ^
-  --entry_point=goog:dragjs.main ^
+  --js='node_modules/google-closure-library/closure/goog/base.js' ^
+  --js='lib/**.js' ^
   --generate_exports ^
   --warning_level=VERBOSE ^
+  --jscomp_error=* ^
+  --extra_annotation_name=listens ^
+  --extra_annotation_name=exportDoc ^
+  --extra_annotation_name=exportInterface ^
   --language_in ECMASCRIPT6_STRICT ^
   --language_out ECMASCRIPT5 ^
   --compilation_level ADVANCED_OPTIMIZATIONS ^
